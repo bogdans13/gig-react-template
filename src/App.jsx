d07b52a2ca56d4cbdd7728d7653713ec51e21cfa
Bogdan <bogdans@me.com>
2024-03-22 17:37:54 +0000

    Second React 'Passing data(props) to Components' - Challenge
    modify Gig.jsx to use props instead; modify App.jsx to remove hardcoded entries and pass props to <Gig /> element instead; duplicate it after to show a second concert as well; add new image to /src/assets for second artist/concert.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,52 @@ import Hello from "./components/Hello";
 import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
 import Gig from "./components/Gig.jsx"
+import tameImpala from "./assets/tame_impala_band.jpg"
+import omarApollo from "./assets/omar_apollo.webp"
+
 // import "./Gig.css"
 // leaving out for the moment, CSS is work in progress
 
+// Code at React: Passing data(props) to Components - Challenge 
+// part in the learning module (changing name/job/dob to use props)
 
 function App() {
   return (
     <div>
       <Hello name="World" />
       <img className="logo" src={makersLogo}></img>
-      <Gig/>
+      <Gig
+        name="Tame Impala"
+        logoSrc={tameImpala}
+        description="lorem ipsum testing sample concert"
+        timeanddate="Thu 21 March 2024 19:00"
+        location="O2 Greenwhich, London"
+      />
+      {/* duplicating the Gig component for a second gig */}
+      <Gig
+        name="Omar Apollo"
+        logoSrc={omarApollo}
+        description="lorem ipsum testing sample concert"
+        timeanddate="Thu 21 March 2024 19:00"
+        location="The RoundHouse, London"
+      />
+
     </div>
-    );
+  );
 }
+
+
+// Code at React: React Components - Exercise part in
+// the learning module (first time constructing Profile)
+
+// function App() {
+//   return (
+//     <div>
+//       <Hello name="World" />
+//       <img className="logo" src={makersLogo}></img>
+//       <Gig/>
+//     </div>
+//     );
+// }
 
 export default App;
--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,19 +1,38 @@
 import React from "react";
-import tameImpala from "../assets/tame_impala_band.jpg";
 
 // "../" because the .jpg is one level up from/outside of 
 // the src/components/ folder; in src/assets folder instead
 
-const Gig = () => {
+// Code at React: Passing data(props) to Components - Challenge 
+// part in the learning module (listing elements as props in Gig)
+
+const Gig = (props) => {
     return (
         <div id="band">
-        <h3 id="name">Tame Impala</h3>
-        <img className="logo" src={tameImpala}></img>
-        <p id="description">lorem ipsum testing sample concert</p>
-        <p id="timeanddate">Thu 21 March 2024 19:00</p>
-        <p id="location">O2 Greenwhich, London</p>
+        <h3 id="name">{props.name}</h3>
+        <img className="logo" src={props.logoSrc}/>
+        <p id="description">{props.description}</p>
+        <p id="timeanddate">{props.timeanddate}</p>
+        <p id="location">{props.location}</p>
         </div>
     )
 };
+
+
+// Code at React: React Components - Challenge part in
+// the learning module (first time constructing Gig)
+
+// const Gig = () => {
+//     return (
+//         <div id="band">
+//         <h3 id="name">Tame Impala</h3>
+//         <img className="logo" src={tameImpala}></img>
+//         <p id="description">lorem ipsum testing sample concert</p>
+//         <p id="timeanddate">Thu 13 April 2024 20:00</p>
+//         <p id="location">O2 Greenwhich, London</p>
+//         </div>
+//     )
+// };
+
 
 export default Gig;
